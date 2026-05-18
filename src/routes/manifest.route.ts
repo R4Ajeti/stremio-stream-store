46f1729538f79f2913fa512d181aeb85a055d69c
Rinor Ajeti <r4ajeti@gmail.com>
2026-05-18 12:50:01 +0200

    Update version numbers in package.json and manifest.route.ts to 1.0.1

--- src/routes/manifest.route.ts
+++ src/routes/manifest.route.ts
@@ -6,7 +6,7 @@ export async function ManifestRoute(App: FastifyInstance) {
   App.get('/manifest.json', async () => {
     return {
       id: Env.ADDON_ID,
-      version: '1.0.0',
+      version: '1.0.1',
       name: Env.ADDON_NAME,
       description: Env.ADDON_DESCRIPTION,
       logo: `${Env.ADDON_BASE_URL}/public/logo.png`,
