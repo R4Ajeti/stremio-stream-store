66d57afb2555db1985c89f8f4808aaef8e789b1a
Rinor Ajeti <r4ajeti@gmail.com>
2026-05-18 02:27:53 +0200

    Update manifest route to dynamically set logo URL based on request headers

--- src/routes/manifest.route.ts
+++ src/routes/manifest.route.ts
@@ -3,13 +3,24 @@ import type { FastifyInstance } from 'fastify'
 import { Env } from '../config/env.js'
 
 export async function ManifestRoute(App: FastifyInstance) {
-  App.get('/manifest.json', async () => {
+  App.get('/manifest.json', async (RequestObj) => {
+    const HostStr = RequestObj.headers.host
+    const ForwardedProtoStr = RequestObj.headers['x-forwarded-proto']
+
+    const ProtocolStr = typeof ForwardedProtoStr === 'string'
+      ? ForwardedProtoStr.split(',')[0]
+      : 'https'
+
+    const BaseUrlStr = HostStr
+      ? `${ProtocolStr}://${HostStr}`
+      : Env.ADDON_BASE_URL
+
     return {
       id: Env.ADDON_ID,
       version: '1.0.0',
       name: Env.ADDON_NAME,
       description: Env.ADDON_DESCRIPTION,
-      logo: `${Env.ADDON_BASE_URL}/public/logo.png`,
+      logo: `${BaseUrlStr}/public/logo.png`,
       resources: ['stream'],
       types: ['movie', 'series'],
       catalogs: [],
