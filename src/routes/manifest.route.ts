e54eb7a664fa67cdc027379cf9c6026d65ce00d8
Rinor Ajeti <r4ajeti@gmail.com>
2026-05-18 02:38:27 +0200

    Refactor manifest route to simplify logo URL assignment by removing dynamic host handling

--- src/routes/manifest.route.ts
+++ src/routes/manifest.route.ts
@@ -3,24 +3,13 @@ import type { FastifyInstance } from 'fastify'
 import { Env } from '../config/env.js'
 
 export async function ManifestRoute(App: FastifyInstance) {
-  App.get('/manifest.json', async (RequestObj) => {
-    const HostStr = RequestObj.headers.host
-    const ForwardedProtoStr = RequestObj.headers['x-forwarded-proto']
-
-    const ProtocolStr = typeof ForwardedProtoStr === 'string'
-      ? ForwardedProtoStr.split(',')[0]
-      : 'https'
-
-    const BaseUrlStr = HostStr
-      ? `${ProtocolStr}://${HostStr}`
-      : Env.ADDON_BASE_URL
-
+  App.get('/manifest.json', async () => {
     return {
       id: Env.ADDON_ID,
       version: '1.0.0',
       name: Env.ADDON_NAME,
       description: Env.ADDON_DESCRIPTION,
-      logo: `${BaseUrlStr}/public/logo.png`,
+      logo: `${Env.ADDON_BASE_URL}/public/logo.png`,
       resources: ['stream'],
       types: ['movie', 'series'],
       catalogs: [],
